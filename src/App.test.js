d823fd2aa95f223c7fd86a91dafa047ea88dc884
Kelvin Liang <kelvin9877@gmail.com>
2020-05-21 00:52:58 +0800

    final touch

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { render, fireEvent, waitForElementToBeRemoved, getByLabelText } from "@testing-library/react";
+import { render, fireEvent, waitForElementToBeRemoved, within } from "@testing-library/react";
 import App from "./App";
 import mockPosts from "./__mocks__/mockPosts.json";
 
 jest.mock('./api');
 
 const weekdays = [
-  // "Sunday",
+  "Sunday",
   "Monday",
-  // "Tuesday",
-  // "Wednesday",
-  // "Thursday",
-  // "Friday",
-  // "Saturday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
 function getPostDay({ createdAt }) {
@@ -25,28 +25,24 @@ mockPosts.sort((a, b) => getPostDay(a) - getPostDay(b));
 test.each(weekdays)(
   "shows table containing correct posts for %s",
   async (weekday) => {
-    const { getByText, getByRole, getAllByRole, getByLabelText, debug } = render(<App />);
-
-    // const weekdayButton = getByText(weekday);
-    // fireEvent.click(weekdayButton);
+    const { getByText, getByRole, getAllByRole, getByLabelText } = render(<App />);
 
     const select = getByLabelText(/Selected weekday/);
     fireEvent.change(select, { target: { value: weekday } });
-
-    const day = weekdays.indexOf(weekday);
-    const postIndex = mockPosts.findIndex((post) => getPostDay(post) === day);
-
     await waitForElementToBeRemoved(() => getByText(/Loading/));
 
-    getByRole("table");
-    const rows = getAllByRole("row");
+    const day = weekdays.indexOf(weekday);
+    getByRole('table');
+    const rows = getAllByRole('row');
     // debug(rows);
 
-    for (let i = 0; i < rows.length - 1; i += 1) {
-      const post = mockPosts[postIndex + i];
-      getByText(post.author);
-      getByText(post.title);
-      getByText(post.score.toString());
-    }
+    const postsForWeekday = mockPosts.filter((post) => getPostDay(post) === day);
+
+    postsForWeekday.forEach((post, index) => {
+      const row = rows[index + 1];
+      within(row).getByText(post.author);
+      within(row).getByText(post.title);
+      within(row).getByText(post.score.toString());
+    });
   }
 );
